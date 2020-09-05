fad685a9c637309d0333f1016dab2da0295e76a3
tobidot <Object.name@live.de>
2020-09-05 18:32:49 +0200

    Extend Test Class to exceptions

--- tests/base/TestClass.ts
+++ tests/base/TestClass.ts
@@ -1,3 +1,5 @@
+import { TestClassExceptionExpectedExcetpion } from "./ExceptionExpectedException";
+
 type Printable<T> = { toString(): string };
 type Class<T> = { new(...args: any): T, name: string };
 type Named<T> = { name: string };
@@ -7,6 +9,16 @@ export class TestClass {
     public tear_down() { }
     public set_class_up() { }
     public tear_class_down() { }
+
+    public assert_exception<E extends Error>(exception: Class<E>, callback: () => void) {
+        try {
+            callback();
+            throw new TestClassExceptionExpectedExcetpion('Expected an exception from ' + String(callback).substr(0, 255));
+        } catch (error) {
+            if (!(error instanceof exception)) throw error;
+            this.success();
+        }
+    }
 
     public assertInstanceOf<T extends Printable<T>>(value: Printable<T>, expected: Class<T>): void {
         if (false === value instanceof expected) {
